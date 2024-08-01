57c1a8c98e72143ccc9e34412a64d9ddcb3a1bca
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-08-01 23:23:54 +0300

    Lists and Keys

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const linked = links.map((link, index) => {
+   return <a href= {`#${link}`} key={index}>{link}</a>
+})
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    {linked}
+  </nav>;
 }
 
 export default NavBar;
